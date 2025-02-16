fe93ff79269fb16f558b493e670b46bf5b6355de
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-16 21:30:22 +0100

    first commit

---
+++ palindromo/main.js
@@ -0,0 +1,17 @@
+// Chiedere all'utente di inserire una parola, creare una funzione per capire se la parola inserita è palindroma 
+
+
+// Procediamo a creare prima la funzione che ci serve
+
+function checkPalindrom(word){
+    let reverseWord = " ";
+    for (let i = word.lenght - 1; i >= 0; i--){
+        reverseWord += word[i];
+    }
+    if (reverseWord === word) {
+        return true
+    } 
+    else {
+        return false;
+    }
+}
